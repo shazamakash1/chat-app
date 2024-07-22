d28ef970db523ff93b53e28da3a64f482d8ceefd
Akash <manirujjamanandlion@gmail.com>
2024-07-22 23:08:00 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
@@ -9,20 +10,28 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import { app, server } from "./socket/socket.js";
 
-dotenv.config();
-
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Backend is UP :)");
-});
+// app.get("/", (req, res) => {
+//   res.send("Backend is UP :)");
+// });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"], // Replace with your frontend URL
+    origin: ["http://localhost:5000"], // Replace with your frontend URL
     methods: ["GET", "POST"],
   },
 });
