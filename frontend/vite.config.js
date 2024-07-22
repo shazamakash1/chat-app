a127738757130fa65e96bcfa7d539fe816eca222
Akash <manirujjamanandlion@gmail.com>
2024-07-22 23:19:00 +0530

    url changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      "/api": { target: "http://localhost:8000" },
+      "/api": { target: "https://chat-app-klpj.onrender.com/" },
     },
   },
 });
